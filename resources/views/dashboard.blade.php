9d223f1fb26d831fe3b6f39a24ccee928f61b229
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2025-05-22 23:21:55 +0600

    Refactor layout styles in dashboard and navigation components for improved responsiveness and consistency

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-4">
-        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-4 text-gray-900">
                     <x-category-tabs >
